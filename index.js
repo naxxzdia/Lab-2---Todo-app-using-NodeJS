e1215b89a2ace03e8e637c44ccbda4be13398b65
Kanokros <6607012663018@email.kmutnb.ac.th>
2024-12-23 12:43:44 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+//MiddleLeware
+app.use(bodyParser.urlencoded({extende: true}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+let todos = [];
+
+//Routes
+app.get('/', (req, res) => {
+    res.render('index', {todos});
+});
+
+app.post('/add', (req, res) => {
+    const newTodo = req.body.todo;
+    if (newTodo) todos.push(newTodo);
+    res.redirect('/');
+});
+
+app.post('/delete', (req, res) => {
+    const index = req.body.index;
+    todos .splice(index, 1);
+    res.redirect('/');
+});
+
+//Start server
+const PORT = 3000;
+app.listen(PORT, ()=> {
+    console.log(`Server is running at http://localhost:${PORT}`)
+});
+
